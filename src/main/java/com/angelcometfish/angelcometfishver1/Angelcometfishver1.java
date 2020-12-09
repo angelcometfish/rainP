20f8c112e2d759340f71be66eb1ce860ed021677
angelcometfish <suzunerrei@gmail.com>
2020-12-09 17:43:41 +0900

    mobの心ドロップの書式追加

--- src/main/java/com/angelcometfish/angelcometfishver1/Angelcometfishver1.java
+++ src/main/java/com/angelcometfish/angelcometfishver1/Angelcometfishver1.java
@@ -1,13 +1,17 @@
 package com.angelcometfish.angelcometfishver1;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public final class Angelcometfishver1 extends JavaPlugin implements Listener {
@@ -23,15 +27,27 @@ public final class Angelcometfishver1 extends JavaPlugin implements Listener {
         // Plugin shutdown logic
     }
     @EventHandler
-    public void onKill(PlayerDeathEvent e){
+    public void onKill(EntityDeathEvent e){
         Random rand = new Random();
-        int num = rand.nextInt(10);
+        int num = rand.nextInt(100);
         Player p = e.getEntity().getKiller();
+        String w = e.getEntity().getName();
         assert p != null;
-        if(num<5){
-            p.sendMessage("5以上です");
-            p.getInventory().addItem(new ItemStack(Material.DIAMOND));
-    }else{
-            p.sendMessage("5以下です");
+        if (1 == num) {
+                p.sendMessage("特に何もないよ");
+            } else {
+                ItemStack myitem = new ItemStack(Material.DIAMOND);
+                String dispname = w+"の心";
+                List lores = new ArrayList();
+                lores.add(w+"の心");
+                lores.add("めっちゃレア");
+                myitem.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 1);
+                ItemMeta im = myitem.getItemMeta();
+                assert im != null;
+                im.setDisplayName(dispname);
+                im.setLore(lores);
+                myitem.setItemMeta(im);
+                p.sendMessage(w + "の心をドロップしました");
+                p.getInventory().addItem(myitem);
         }
-}}
+    }}
